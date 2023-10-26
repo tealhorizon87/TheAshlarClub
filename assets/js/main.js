84f34b1ac3e46cad158c5c5d29d8f89fd074ef62
Neal Toms <nealtoms@gmail.com>
2023-10-26 17:42:40 +0100

    add js file, move tooltips js and add about text

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// bootstrap tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+// insert text for about page from .txt file
+const container = document.getElementById("aboutContent");
+
+function getText() {
+    var myRequest = new Request("../assets/text/about_text.txt");
+    fetch(myRequest)
+        .then((response) => response.text())
+        .then((text) => {
+            container.innerHTML = text;
+        });
+}
+
+getText();
