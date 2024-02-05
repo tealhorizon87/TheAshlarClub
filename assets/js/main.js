8a15f72c497a894c5c4029341364072b917c9089
Neal Toms <nealtoms@gmail.com>
2024-02-05 14:42:57 +0000

    finalise emailJS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,4 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
