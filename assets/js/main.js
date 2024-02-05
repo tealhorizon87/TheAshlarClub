d33b50b11534e98d0bc5741b9bc5c6291b78d75e
Neal Toms <nealtoms@gmail.com>
2024-02-05 13:46:41 +0000

    last chance for emailJS before going a different direction

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,3 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
-
-function sendMail(form) {
-  var formData = {
-      "name": form.name.value,
-      "email": form.email.value,
-      "subject": form.email.value,
-      "query": form.query.value,
-  };
-
-  emailjs.send("service_dgna3zv", "template_jh1yc7f", formData)
-  .then(
-    function(response) {
-      window.location.href = "thanks.html";
-      return response;
-    },
-    function(error) {
-      alert("I'm sorry, something went wrong! Please try again");
-      return error;
-    });
-    
-  return false;
-}
