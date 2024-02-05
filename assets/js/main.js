1d29faf16496b644263a219a0b157756b2091ab5
Neal Toms <nealtoms@gmail.com>
2024-02-05 12:25:24 +0000

    Adjust contact form and script to get contact form working

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,41 @@ return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 
-function sendMail(queryForm) {
-  var formData = {
-      "name": queryForm.name.value,
-      "email": queryForm.email.value,
-      "subject": queryForm.email.value,
-      "query": queryForm.query.value,
-  };
+// function sendMail(queryForm) {
+//   var formData = {
+//       "name": queryForm.name.value,
+//       "email": queryForm.email.value,
+//       "subject": queryForm.email.value,
+//       "query": queryForm.query.value,
+//   };
 
-  emailjs.send("service_dgna3zv", "template_jh1yc7f", formData)
-  .then(
-    function(response) {
-      window.location.href = "thanks.html";
-      return response;
-    },
-    function(error) {
-      alert("I'm sorry, something went wrong! Please try again");
-      return error;
-    });
+//   emailjs.send("service_dgna3zv", "template_jh1yc7f", formData)
+//   .then(
+//     function(response) {
+//       window.location.href = "thanks.html";
+//       return response;
+//     },
+//     function(error) {
+//       alert("I'm sorry, something went wrong! Please try again");
+//       return error;
+//     });
     
-  return false;
+//   return false;
+// }
+
+window.onload = function() {
+  document.getElementById('queryForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm("service_dgna3zv", "template_jh1yc7f", this)
+      .then(
+        function(response) {
+          window.location.href = "thanks.html";
+          return response;
+        },
+        function(error) {
+          alert("I'm sorry, something went wrong! Please try again");
+          return error;
+        });
+  });
 }
